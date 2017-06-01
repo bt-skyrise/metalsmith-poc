6f5c39d6916127a163f64180f0350febbaa42dc0
redss <ryszard.tarajkowski@gmail.com>
2017-06-01 13:45:44 +0200

    Build in production/development mode

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-const metalsmith = require('metalsmith');
+const Metalsmith = require('metalsmith');
 const layouts = require('metalsmith-layouts');
 const sass = require('metalsmith-sass');
 const static = require('metalsmith-static');
@@ -6,27 +6,37 @@ const inPlace = require('metalsmith-in-place');
 const serve = require('metalsmith-serve');
 const watch = require('metalsmith-watch');
 
-metalsmith(__dirname)
+const command = process.argv[2];
+const development = command === 'watch';
+
+const metalsmith = Metalsmith(__dirname)
     .metadata({
-        foo: 'bar'
+        foo: 'bar',
+        development: development
     })
     .source('./src')
     .destination('./build')
-    .clean(true)
-    .use(watch({
-        livereload: true,
-        paths: {
-            "${source}/**/*": true,
-            "layouts/**/*": "**/*"
-        }
-    }))
-    .use(serve())
+    .clean(true);
+
+if (development) {
+    metalsmith
+        .use(watch({
+            livereload: true,
+            paths: {
+                "${source}/**/*": true,
+                "layouts/**/*": "**/*"
+            }
+        }))
+        .use(serve());
+}
+
+metalsmith
     .use(static({
         src: "node_modules/bootstrap-sass/assets/fonts/bootstrap",
         dest: "fonts/bootstrap"
     }))
-    .use(inPlace())
     .use(sass())
+    .use(inPlace())
     .use(layouts('handlebars'))
     .build(err => {
         if (err) {
