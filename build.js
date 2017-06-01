161affcee10ad6e0a7047aa7de171a9113b8110c
redss <ryszard.tarajkowski@gmail.com>
2017-06-01 11:27:12 +0200

    Included sass and bootstrap 3.

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const Metalsmith = require('metalsmith');
 const layouts = require('metalsmith-layouts');
-const inPlace = require('metalsmith-in-place');
+const sass = require('metalsmith-sass');
 
 Metalsmith(__dirname)
     .metadata({
@@ -9,6 +9,7 @@ Metalsmith(__dirname)
     .source('./src')
     .destination('./build')
     .clean(true)
+    .use(sass())
     .use(layouts({
         engine: 'handlebars'
     }))
